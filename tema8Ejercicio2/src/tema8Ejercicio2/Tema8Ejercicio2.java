8e98e1a255d558fb81bb3781fe37b959e802f89e
Daniel <danielzanon2005@gmail.com>
2025-03-11 10:06:57 +0100

    Acabado

--- tema8Ejercicio2/src/tema8Ejercicio2/Tema8Ejercicio2.java
+++ tema8Ejercicio2/src/tema8Ejercicio2/Tema8Ejercicio2.java
@@ -43,6 +43,7 @@ public class Tema8Ejercicio2 {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		//Estas tres lineas sirven para que el programa Java reconozca la base de datos
 		String url="jdbc:mysql://127.0.0.1:3307/comunidades";
 		String user="alumno";
 		String password="Alumno123";
@@ -59,9 +60,15 @@ public class Tema8Ejercicio2 {
 			lblComunidadAutonoma.setBounds(12, 12, 168, 15);
 			frame.getContentPane().add(lblComunidadAutonoma);
 			
+			JLabel lblProvincias = new JLabel("Provincias");
+			lblProvincias.setBounds(205, 12, 168, 15);
+			frame.getContentPane().add(lblProvincias);
+			
 			JComboBox comboBox_ComunidadesAutonomas = new JComboBox();
+			JComboBox comboBoxProvincias = new JComboBox();
+			
 			comboBox_ComunidadesAutonomas.setBounds(12, 39, 168, 24);
-			try {
+			try { //Este try sirve para que, de la tabla comunidad, ponga todos los nombres en el primer comboBox
 				Statement stmt = con.createStatement();
 				ResultSet rs = stmt.executeQuery("SELECT * FROM comunidad");
 				while (rs.next()) {
@@ -74,23 +81,31 @@ public class Tema8Ejercicio2 {
 			} catch (SQLException e2) {
 				e2.printStackTrace();
 			}
+			
+			comboBox_ComunidadesAutonomas.addItemListener(new ItemListener() {
+				public void itemStateChanged(ItemEvent e) { //Esta propiedad es para cuando eliges otra comunidad del primer comboBox
+					
+					//Este primer try sirve para que, si elijes la comunidad de Valencia, te ponga en el segundo comboBox todas sus provincias
+					try {
+						PreparedStatement sel_pstmt = con.prepareStatement("SELECT * FROM provincia WHERE provincia.id_comunidad=?");
+						sel_pstmt.setInt(1, comboBox_ComunidadesAutonomas.getSelectedIndex()+1);
+						ResultSet rs_sel = sel_pstmt.executeQuery();
+						comboBoxProvincias.removeAllItems();
+						while (rs_sel.next()) {
+							String nombre=rs_sel.getString("nombre");
+							comboBoxProvincias.addItem(nombre);
+						}
+						rs_sel.close();
+						sel_pstmt.close();
+					} catch (SQLException e2) {
+						e2.printStackTrace();
+					}
+				}
+			});
 			frame.getContentPane().add(comboBox_ComunidadesAutonomas);
 			
-			JLabel lblProvincias = new JLabel("Provincias");
-			lblProvincias.setBounds(205, 12, 168, 15);
-			frame.getContentPane().add(lblProvincias);
-			
-			JComboBox comboBoxProvincias = new JComboBox();
 			comboBoxProvincias.setBounds(205, 39, 168, 24);
-			try {
-				Statement stmt = con.createStatement();
-				ResultSet rs = stmt.executeQuery("SELECT * FROM comunidad");
-				while (rs.next()) {
-					int idComunidad=rs.getInt("id");
-				}
-				rs.close();
-				stmt.close();
-				
+			try { //Este try sirve para que, de la tabla provincia, ponga todos los nombres en el segundo comboBox
 				PreparedStatement sel_pstmt = con.prepareStatement("SELECT * FROM provincia WHERE provincia.id_comunidad=?");
 				sel_pstmt.setInt(1, comboBox_ComunidadesAutonomas.getSelectedIndex()+1);
 				ResultSet rs_sel = sel_pstmt.executeQuery();
